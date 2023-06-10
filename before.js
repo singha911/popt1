721ced7d103a4b296f76949dd8504b5253ec69ef
singha911 <135771908+singha911@users.noreply.github.com>
2023-06-10 08:59:19 +0530

    Delete before.js

--- before.js
+++
@@ -1,1 +0,0 @@
-window.addEventListener("beforeunload",(function(e){var n="It looks like you have been editing something. If you leave before saving, your changes will be lost.";return(e||window.event).returnValue=n,n}));
